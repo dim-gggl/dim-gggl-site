2aacc06d298bb016b587519376c77ddc1550f4f4
dim-gggl <dimitri.gaggioli@gmail.com>
2025-10-30 22:28:09 +0100

    Added favicon

--- portfolio_dimitri/settings.py
+++ portfolio_dimitri/settings.py
@@ -222,7 +222,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage,
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
     },
 }
 
